477e5cab44488c70efde1f2639ac40dec874c1ae
astadoreinoclover <renatoicardozo2001@gmail.com>
2023-05-04 21:52:50 -0300

    Banner-pronto

---
+++ resources/js/script.js
@@ -0,0 +1,9 @@
+function abrirMenu() {
+    if(document.querySelector('nav').classList.contains('none')) {
+        document.querySelector('nav').classList.remove('none');
+        document.querySelector('nav').classList.add('flex');
+    } else {
+        document.querySelector('nav').classList.remove('flex');
+        document.querySelector('nav').classList.add('none');
+    }
+}
