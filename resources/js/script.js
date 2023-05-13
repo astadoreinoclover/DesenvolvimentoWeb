18ce26b5cd82c063170b77b966039445420e5a83
astadoreinoclover <renatoicardozo2001@gmail.com>
2023-05-13 14:59:46 -0300

    Filmes

--- resources/js/script.js
+++ resources/js/script.js
@@ -2,50 +2,13 @@ function abrirMenu() {
     if(document.querySelector('nav').classList.contains('none')) {
         document.querySelector('nav').classList.remove('none');
         document.querySelector('nav').classList.add('flex');
+        document.querySelector('.controls').classList.add('mb')
     } else {
         document.querySelector('nav').classList.remove('flex');
         document.querySelector('nav').classList.add('none');
+        document.querySelector('.controls').classList.remove('mb')
     }
 }
-
-// let banner = 1;
-
-// function proximo() {
-//     if (banner < 4) {
-//         banner = banner + 1;
-//     } else {
-//         banner = 1;
-//     }
-
-//     const bannerAtual = document.querySelector(`.banner-${banner}`);
-//     const bannerAnterior = document.querySelector(`.banner-${banner-1}`);
-//     bannerAtual.classList.remove('none');
-//     bannerAtual.classList.add('banner');
-//     bannerAnterior.classList.remove('banner');
-//     bannerAnterior.classList.add('none');
-//     document.querySelector(`#banner-${banner-1}`).classList.remove('active');
-//     document.querySelector(`#banner-${banner}`).classList.add('active');
-
-//     console.log(banner);
-// }
-
-// function anterior() {
-//     if (banner > 1) {
-//         banner = banner - 1;
-//     } else {
-//         banner = 4;
-//     }
-
-//     const bannerAtual = document.querySelector(`.banner-${banner}`);
-//     const bannerAnterior = document.querySelector(`.banner-${banner+1}`);
-//     bannerAtual.classList.remove('none');
-//     bannerAtual.classList.add('banner');
-//     bannerAnterior.classList.remove('banner');
-//     document.querySelector(`#banner-${banner+1}`).classList.remove('active');
-//     document.querySelector(`#banner-${banner}`).classList.add('active');
-
-//     console.log(banner);
-// }
 
 let banner = 1;
 
@@ -57,10 +20,13 @@ function proximo() {
     }
     const bannerAtual = document.querySelector(`.banner-${banner}`);
     const bannerAnterior = document.querySelector(`.banner-${banner === 1 ? 4 : banner - 1}`);
-    bannerAtual.classList.remove('none');
+    bannerAtual.classList.remove('b-none');
     bannerAtual.classList.add('banner');
     bannerAnterior.classList.remove('banner');
-    bannerAnterior.classList.add('none');
+    bannerAnterior.classList.add('b-none');
+    //======================================================
+    document.querySelector(`#banner-${banner === 1 ? 4 : banner - 1}`).classList.remove('active');
+    document.querySelector(`#banner-${banner}`).classList.add('active');
 }
 
 function anterior() {
@@ -71,11 +37,14 @@ function anterior() {
     }
     const bannerAtual = document.querySelector(`.banner-${banner}`);
     const bannerAnterior = document.querySelector(`.banner-${banner === 4 ? 1 : banner + 1}`);
-    bannerAtual.classList.remove('none');
+    bannerAtual.classList.remove('b-none');
     bannerAtual.classList.add('banner');
     bannerAnterior.classList.remove('banner');
-    bannerAnterior.classList.add('none');
+    bannerAnterior.classList.add('b-none');
     // =====================================================
-    
+    document.querySelector(`#banner-${banner === 4 ? 1 : banner + 1}`).classList.remove('active');
+    document.querySelector(`#banner-${banner}`).classList.add('active');
 }
 
+
+setInterval(proximo, 6000);
